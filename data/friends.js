07700a18060b0af017b2eb42b62592cd9b73370a
Drakeen <info@howardfms.com>
2019-01-24 23:40:18 -0700

    layout design etc

---
+++ data/friends.js
@@ -0,0 +1,84 @@
+
+var friends = [
+    {
+        "name":"Gimli, son of Glóin",
+        "photo":"https://vignette.wikia.nocookie.net/lotr/images/e/ec/Gimli_-_FOTR.png/revision/latest?cb=20121008105956",
+        "scores":[
+            1,
+            1,
+            5,
+            3,
+            5,
+            1,
+            5,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Gandalf Stormcrow",
+        "photo":"https://vignette.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754",
+        "scores":[
+            5,
+            1,
+            1,
+            5,
+            3,
+            5,
+            1,
+            5,
+            5,
+            1
+          ]
+      },
+      {
+        "name":"Aragorn son of Arathorn",
+        "photo":"https://vignette.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423",
+        "scores":[
+            3,
+            5,
+            3,
+            5,
+            1,
+            1,
+            1,
+            1,
+            5,
+            1
+          ]
+      },
+      {
+        "name":"Saruman",
+        "photo":"https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833",
+        "scores":[
+            5,
+            1,
+            3,
+            3,
+            3,
+            5,
+            1,
+            1,
+            1,
+            5
+          ]
+      },
+      {
+        "name":"Peregrin Took",
+        "photo":"https://vignette.wikia.nocookie.net/lotr/images/0/0a/Pippinprintscreen.jpg/revision/latest?cb=20060310083048",
+        "scores":[
+            5,
+            3,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            3,
+            1
+          ]
+      }
+
+]
