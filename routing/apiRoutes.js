64eea5433fc7d2cce73545a2fb7c3f61a9cdd968
Drakeen <info@howardfms.com>
2019-01-25 22:02:02 -0700

    form submission and saving results to server etc

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,12 +1,14 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(friendData);
+        res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
+        friends.push(req.body);
+        //send callback to survey file to clear the form
+        res.json(true);
     })
 
 }
